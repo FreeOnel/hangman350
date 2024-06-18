b20e32468e43fed9f4e9d2f5e6fd4c5ec834f6eb
scaayao-rockpool <steve.caayao@rockpooldigital.com>
2024-06-18 17:45:58 +0100

    Milestone 4 Completed

---
+++ milestone_4/milestone_4.py
@@ -0,0 +1,37 @@
+import random as rd
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5) -> None:
+        self.word = rd.choice(word_list)
+        self.word_guessed = ['_']*len(self.word)
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = []
+
+    def check_guess(self, guess: str):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            for i, letter in enumerate(self.word):
+                if letter == guess:
+                    self.word_guessed[i] = guess
+            
+            self.num_letters -= 1
+
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Please guess a letter: ")
+            if not(guess.isalpha() and len(guess) == 1):
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+
